ec4d23016b90378c95f66ba139cc64057c4dac92
SHUKLA Vinay <vinay.shukla@alstomgroup.com>
2018-07-01 21:41:09 +0530

    changes in first.c and third.h from branch1

--- first.c
+++ first.c
@@ -4,5 +4,6 @@ int main()
 {
 	printf("write something in first file%s\n");
 	printf("upadted soemthing for branch%s\n");
+	printf("upadted soemthing for branch%s\n");
 	return 0;
 }
--- third.h
+++ third.h
@@ -2,6 +2,7 @@
 #define THIRD_H_
 
 #define SEVEN 7
+#define SEVENTY 70
 typedef unsigned long uint_32;
 
 
